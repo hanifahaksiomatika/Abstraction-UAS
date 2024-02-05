8f5b28466ad9e145bca645de481550bdb6ebacec
hanifahaksiomatika <128040561+hanifahaksiomatika@users.noreply.github.com>
2024-02-06 05:40:37 +0700

    Add files via upload

---
+++ UAS_Abstraction/UAS_Abstraction/Fashion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAS_Abstraction
+{
+    public abstract class Fashion
+    {
+        public abstract void Sepatu();
+    }
+    public class Sneakers : Fashion
+    {
+        public override void Sepatu()
+        {
+            Console.WriteLine("Sneakers biasa digunakan dalam kegiatan casual");
+            Console.WriteLine("Digemari oleh kalangan anak muda");
+        }
+    }
+    public class Pantofel : Fashion
+    {
+        public override void Sepatu()
+        {
+            Console.WriteLine("Pantofel biasa digunakan dalam kegiatan Formal");
+            Console.WriteLine("Pekerja kantoran biasanya menggunakan sepatu ini");
+        }
+    }
+    public class Heels : Fashion
+    {
+        public override void Sepatu()
+        {
+            Console.WriteLine("Heels biasa digunakan dalam kegiatan semi-formal");
+            Console.WriteLine("Digemari oleh kalangan wanita");
+        }
+    }
+}
---
+++ UAS_Abstraction/UAS_Abstraction/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UAS_Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fashion fashion;   //Abstract class
+
+            Console.WriteLine();
+            fashion = new Sneakers();
+            fashion.Sepatu();
+
+            Console.WriteLine();
+            fashion = new Pantofel();
+            fashion.Sepatu();
+
+            Console.WriteLine();
+            fashion = new Heels();
+            fashion.Sepatu();
+
+            Console.ReadKey();
+
+            /*IFashion fashion;         // menggunakan Interface
+             
+             Console.WriteLine();
+
+             fashion = new Sneakers();
+             fashion.Sepatu();
+             Console.WriteLine();
+             
+             fashion = new Pantofel();
+             fashion.Sepatu();
+             Console.WriteLine();
+             
+             fashion = new Heels();
+             fashion.Sepatu();
+             Console.WriteLine();*/
+
+            Console.ReadKey();
+        }
+    }
+}
